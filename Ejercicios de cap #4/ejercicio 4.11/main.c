d5a2a188addf5ad38d6be6f6d363d983cf067b49
RamirezFamilia <134010530+RamirezFamilia@users.noreply.github.com>
2023-07-28 19:46:37 -0400

    Add files via upload

---
+++ Ejercicios de cap #4/ejercicio 4.11/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int);
+
+void main(void)
+{
+    int NUM, RES;
+    printf("\nIngresa el numero: ");
+    scanf("%d", &NUM);
+    RES = mad(NUM);
+    printf("\nEl mayor divisor de %d es: %d", NUM, RES);
+}
+
+
+int mad(int N1)
+
+{
+    int I = (N1 / 2);
+    while (N1 % I)
+        I--;
+        return 0;
+}
+
+
+
